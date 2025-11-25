88dbc2eda8c72880a4f173ca69f5f79c23497b93
ethannCB <ethann.campionbaux@gmail.com>
2025-11-25 09:12:08 +0100

    création docstring

--- carte.c
+++ carte.c
@@ -3,13 +3,16 @@
 #include <string.h>
 
 void afficherCarte(Carte carte) {
+    //Fonction qui affiche la carte
     printf("%s %c\n", carte.type, carte.couleur);
 }
 
 int creerJeuUNO(Carte *jeu) {
+    //Fonction qui crée le deck du uno
     int index = 0;
     char couleurs[4] = {'R', 'J', 'B', 'V'};
     
+    //Création des cartes de base
     for (int c = 0; c < 4; c++) {
         jeu[index].type[0] = '0';
         jeu[index].type[1] = '\0';
@@ -24,7 +27,7 @@ int creerJeuUNO(Carte *jeu) {
                 index++;
             }
         }
-
+        //Création des cartes spéciales
         const char *speciaux[] = {"+2", "RV", "PS"};
         for (int j = 0; j < 3; j++) {
             for (int ex = 0; ex < 2; ex++) {
@@ -51,6 +54,7 @@ int creerJeuUNO(Carte *jeu) {
 
 
 void MelangerJeu(Carte *jeu, int taille) {
+    //fonction qui return la fonction mélangé
     for (int i = taille - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         Carte temp = jeu[i];
@@ -60,5 +64,6 @@ void MelangerJeu(Carte *jeu, int taille) {
 }
 
 void affiche_carte(int index_jeu, Carte *jeu) {
+    //fonction qui affiche une carte à un index donné   
     printf("%s %c\n", jeu[index_jeu].type, jeu[index_jeu].couleur);
 }
